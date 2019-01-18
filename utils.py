541d6befcf0446d5c4fdc239e01c5732aaa928a8
jaysonph <40629085+jaysonph@users.noreply.github.com>
2019-01-18 16:01:28 +0800

    Losses and accuracy metrics

---
+++ utils.py
@@ -0,0 +1,61 @@
+import numpy as np
+import tensorflow as tf
+import keras
+from keras import backend as K
+
+
+def iou_loss_core(y_true, y_pred, smooth=1):
+    intersection = K.sum(K.abs(y_true * y_pred), axis=-1)
+    union = K.sum(y_true,-1) + K.sum(y_pred,-1) - intersection
+    iou = (intersection + smooth) / ( union + smooth)
+    return iou
+
+def iou_loss(y_true, y_pred):
+    return 1 - iou_loss_core(y_true, y_pred)
+
+def iou_bce_loss(y_true, y_pred):
+    return keras.losses.binary_crossentropy(y_true, y_pred) + (5 * iou_loss(y_true, y_pred))
+  
+def castF(x):
+    return K.cast(x, K.floatx())
+
+def castB(x):
+    return K.cast(x, bool)
+  
+def IOU_metric(true, pred):
+
+    tresholds = [0.5 + (i*.05)  for i in range(10)]
+
+    #flattened images (batch, pixels)
+    true = K.batch_flatten(true)
+    pred = K.batch_flatten(pred)
+    pred = castF(K.greater(pred, 0.5))
+
+    #total white pixels - (batch,)
+    trueSum = K.sum(true, axis=-1)
+    predSum = K.sum(pred, axis=-1)
+
+    #has mask or not per image - (batch,)
+    true1 = castF(K.greater(trueSum, 1))    
+    pred1 = castF(K.greater(predSum, 1))
+
+    #to get images that have mask in both true and pred
+    truePositiveMask = castB(true1 * pred1)
+
+    #separating only the possible true positives to check iou
+    testTrue = tf.boolean_mask(true, truePositiveMask)
+    testPred = tf.boolean_mask(pred, truePositiveMask)
+
+    #getting iou and threshold comparisons
+    iou = iou_loss_core(testTrue,testPred) 
+    truePositives = [castF(K.greater(iou, tres)) for tres in tresholds]
+
+    #mean of thressholds for true positives and total sum
+    truePositives = K.mean(K.stack(truePositives, axis=-1), axis=-1)
+    truePositives = K.sum(truePositives)
+
+    #to get images that don't have mask in both true and pred
+    trueNegatives = (1-true1) * (1 - pred1) # = 1 -true1 - pred1 + true1*pred1
+    trueNegatives = K.sum(trueNegatives) 
+
+    return (truePositives + trueNegatives) / castF(K.shape(true)[0])
